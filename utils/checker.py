b399bcb84d285c97b65db15bab3008b0dcadcb94
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-05 10:37:54 +0100

    Add convenience method to check expressions and substitutions, formatting.

--- utils/checker.py
+++ utils/checker.py
@@ -2,25 +2,34 @@ import collections
 import logging
 import resource
 import subprocess
+import sys
 import time
 
-from . import options
+from utils import options
+from utils import parser
+from utils import smtlib
+from utils import tmpfiles
 
 RunInfo = collections.namedtuple("RunInfo", ["exit", "out", "err", "runtime"])
 
 __GOLDEN = None
 __GOLDEN_CC = None
 
+
 def limit_memory():
     """Apply memory limit given by :code:`--memout`."""
     if options.args().memout != 0:
-        resource.setrlimit(resource.RLIMIT_AS, (options.args().memout * 1024 * 1024, resource.RLIM_INFINITY))
+        resource.setrlimit(
+            resource.RLIMIT_AS,
+            (options.args().memout * 1024 * 1024, resource.RLIM_INFINITY))
+
 
 def execute(cmd, filename, timeout):
     """Execute the command on the file with a timeout."""
     proc = subprocess.Popen(cmd + [filename],
                             stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE, preexec_fn = limit_memory)
+                            stderr=subprocess.PIPE,
+                            preexec_fn=limit_memory)
     try:
         start = time.time()
         out, err = proc.communicate(timeout=timeout)
@@ -63,13 +72,46 @@ def check(filename):
         return False
 
     if options.args().cmd_cc:
-        ri = execute(options.args().cmd_cc, filename, options.args().timeout_cc)
+        ri = execute(options.args().cmd_cc, filename,
+                     options.args().timeout_cc)
         if not matches_golden(__GOLDEN_CC, ri,
                               options.args().match_out_cc,
                               options.args().match_err_cc):
             return False
 
     return True
+
+
+def check_exprs(exprs):
+    """Run the check on the given expressions. Returns (True,runtime) if the
+    check was successful and (False,0) otherwise."""
+    tmpfile = tmpfiles.get_tmp_filename()
+    parser.write_smtlib_to_file(tmpfile, exprs)
+
+    runtime = 0
+    start = time.time()
+    if check(tmpfile):
+        runtime = time.time() - start
+        return True, runtime
+
+    return False, 0
+
+
+def check_substitution(exprs, subst):
+    """Run the check on the given expressions with substitutions applied.
+    Returns (nreduced,test_exprs,runtime) if the check was successful where
+    nreduced is the number of removed nodes and test_exprs the new expressions.
+    Otherwise returns (0,[],0)."""
+    test_exprs = subst.apply(exprs)
+
+    success, runtime = check_exprs(test_exprs)
+
+    nreduced = 0
+    if success:
+        nreduced = smtlib.node_count(exprs) - smtlib.node_count(test_exprs)
+        return nreduced, test_exprs, runtime
+
+    return 0, [], 0
 
 
 def do_golden_runs():
@@ -95,12 +137,14 @@ def do_golden_runs():
 
     if options.args().match_out:
         if options.args().match_out not in __GOLDEN.out:
-            logging.error('Expected stdout to match "{}"'.format(options.args().match_out))
+            logging.error('Expected stdout to match "{}"'.format(
+                options.args().match_out))
             sys.exit(1)
 
     if options.args().match_err:
         if options.args().match_err not in __GOLDEN.err:
-            logging.error('Expected stderr to match "{}"'.format(options.args().match_err))
+            logging.error('Expected stderr to match "{}"'.format(
+                options.args().match_err))
             sys.exit(1)
 
     if options.args().timeout is None:
@@ -123,8 +167,8 @@ def do_golden_runs():
         if options.args().match_err_cc:
             logging.info("match (cc) (stderr): '{}'".format(
                 options.args().match_err_cc))
-        
+
         if options.args().timeout_cc is None:
             options.args().timeout_cc = (__GOLDEN_CC.runtime + 1) * 1.5
-            logging.info('automatic timeout (cc): {}'.format(options.args().timeout_cc))
-
+            logging.info('automatic timeout (cc): {}'.format(
+                options.args().timeout_cc))
