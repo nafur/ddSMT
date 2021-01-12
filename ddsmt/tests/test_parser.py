20858c0fd8d2036c937d631174f5cecabf9d04f8
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-12 12:27:57 +0100

    Move parser into nodes.py

--- ddsmt/tests/test_parser.py
+++ ddsmt/tests/test_parser.py
@@ -1,8 +1,8 @@
-from ..nodes import Node, dfs, substitute
+from ..nodes import Node, dfs, substitute, parse_smtlib
 from .. import parser
 
 
 def test_basic():
-    assert list(parser.parse_smtlib('(reset)')) == [Node(Node('reset'))]
+    assert list(parse_smtlib('(reset)')) == [Node(Node('reset'))]
     assert list(
-        parser.parse_smtlib('(assert (> x y))')) == [Node(Node('assert'), Node(Node('>'), Node('x'), Node('y')))]
+        parse_smtlib('(assert (> x y))')) == [Node(Node('assert'), Node(Node('>'), Node('x'), Node('y')))]
